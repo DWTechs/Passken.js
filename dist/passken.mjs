b4039ffc60f55e9bfd209a18792be99d3e147d43
LCluber <ludovic.cluber@gmail.com>
2024-11-27 22:16:31 +0100

    test are passing

--- dist/passken.mjs
+++ dist/passken.mjs
@@ -27,121 +27,131 @@ https://github.com/DWTechs/Passken.js
 import { getHashes, randomBytes, pbkdf2Sync, createHmac } from 'node:crypto';
 import { isValidInteger, isString, isBoolean } from '@dwtechs/checkard';
 
-var digests = getHashes();
-var digest = "sha256";
-var keyLen = 64;
-var saltRnds = 12;
+const digests = getHashes();
+let digest = "sha256";
+let keyLen = 64;
+let saltRnds = 12;
 function getSaltRounds() {
-  return saltRnds;
+    return saltRnds;
 }
 function setSaltRounds(rnds) {
-  if (!isValidInteger(rnds, 12, 100, true)) return false;
-  saltRnds = rnds;
-  return saltRnds;
+    if (!isValidInteger(rnds, 12, 100, true))
+        return false;
+    saltRnds = rnds;
+    return saltRnds;
 }
 function getKeyLen() {
-  return keyLen;
+    return keyLen;
 }
 function setKeyLen(len) {
-  if (!isValidInteger(len, 2, 256, true)) return false;
-  keyLen = len;
-  return keyLen;
+    if (!isValidInteger(len, 2, 256, true))
+        return false;
+    keyLen = len;
+    return keyLen;
 }
 function getDigest() {
-  return digest;
+    return digest;
 }
 function setDigest(func) {
-  if (!digests.includes(func)) return false;
-  digest = func;
-  return digest;
+    if (!digests.includes(func))
+        return false;
+    digest = func;
+    return digest;
 }
 function getDigests() {
-  return digests;
+    return digests;
 }
 function hash(pwd, secret) {
-  return createHmac(digest, secret).update(pwd).digest("hex");
+    return createHmac(digest, secret).update(pwd).digest("hex");
 }
 function pbkdf2(pwd, secret, salt) {
-  return pbkdf2Sync(hash(pwd, secret), salt, saltRnds, keyLen, digest).toString("hex");
+    return pbkdf2Sync(hash(pwd, secret), salt, saltRnds, keyLen, digest).toString("hex");
 }
 function encrypt(pwd, secret) {
-  if (!isString(pwd, true) || !isString(secret, true)) return false;
-  var salt = randomBytes(16).toString('hex');
-  return salt + pbkdf2(pwd, secret, salt);
+    if (!isString(pwd, true) || !isString(secret, true))
+        return false;
+    const salt = randomBytes(16).toString('hex');
+    return salt + pbkdf2(pwd, secret, salt);
 }
 function compare(pwd, hash, secret) {
-  if (!isString(pwd, true) || !isString(secret, true)) return false;
-  var hashedPwd = pbkdf2(pwd, secret, hash.slice(0, 32));
-  var storedHash = hash.slice(32);
-  return hashedPwd === storedHash;
+    if (!isString(pwd, true) || !isString(secret, true))
+        return false;
+    const hashedPwd = pbkdf2(pwd, secret, hash.slice(0, 32));
+    const storedHash = hash.slice(32);
+    return hashedPwd === storedHash;
 }
 
-var defOpts = {
-  len: 12,
-  num: true,
-  ucase: true,
-  lcase: true,
-  sym: false,
-  strict: true,
-  exclSimilarChars: true
+const defOpts = {
+    len: 12,
+    num: true,
+    ucase: true,
+    lcase: true,
+    sym: false,
+    strict: true,
+    exclSimilarChars: true,
 };
-var list = {
-  lcase: 'abcdefghijklmnopqrstuvwxyz',
-  ucase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-  num: '0123456789',
-  sym: '!@#$%&*_-+:;?><,./'
+const list = {
+    lcase: 'abcdefghijklmnopqrstuvwxyz',
+    ucase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    num: '0123456789',
+    sym: '!@#$%&*_-+:;?><,./',
 };
-function create(opts) {
-  if (opts === void 0) {
-    opts = defOpts;
-  }
-  var len = isValidInteger(opts.len, 12, 64, true) ? opts.len : defOpts.len;
-  var num = isBoolean(opts.num) ? opts.num : defOpts.num;
-  var ucase = isBoolean(opts.ucase) ? opts.ucase : defOpts.ucase;
-  var sym = isBoolean(opts.sym) ? opts.sym : defOpts.sym;
-  var strict = isBoolean(opts.strict) ? opts.strict : defOpts.strict;
-  var exclSimilarChars = opts.exclSimilarChars ? opts.exclSimilarChars : defOpts.exclSimilarChars;
-  var lcase = isBoolean(opts.lcase) ? opts.lcase : defOpts.lcase;
-  if (!lcase && !num) lcase = true;
-  var chars = [];
-  if (lcase) chars.push.apply(chars, list.lcase);
-  if (ucase) chars.push.apply(chars, list.ucase);
-  if (num) chars.push.apply(chars, list.num);
-  if (sym) chars.push.apply(chars, list.sym);
-  if (exclSimilarChars) {
-    chars.splice(chars.indexOf('l'), 1);
-    chars.splice(chars.indexOf('I'), 1);
-    chars.splice(chars.indexOf('1'), 1);
-    chars.splice(chars.indexOf('o'), 1);
-    chars.splice(chars.indexOf('O'), 1);
-    chars.splice(chars.indexOf('0'), 1);
-  }
-  if (strict) {
-    var pwd = [];
-    if (lcase) pwd.push(getRandChar(list.lcase));
-    if (ucase) pwd.push(getRandChar(list.ucase));
-    if (num) pwd.push(getRandChar(list.num));
-    if (sym) pwd.push(getRandChar(list.sym));
-    for (var i = pwd.length; i < len; i++) {
-      pwd.push(getRandChar(chars.join('')));
+function create(opts = defOpts) {
+    const len = isValidInteger(opts.len, 12, 64, true) ? opts.len : defOpts.len;
+    const num = isBoolean(opts.num) ? opts.num : defOpts.num;
+    const ucase = isBoolean(opts.ucase) ? opts.ucase : defOpts.ucase;
+    const sym = isBoolean(opts.sym) ? opts.sym : defOpts.sym;
+    const strict = isBoolean(opts.strict) ? opts.strict : defOpts.strict;
+    const exclSimilarChars = opts.exclSimilarChars ? opts.exclSimilarChars : defOpts.exclSimilarChars;
+    let lcase = isBoolean(opts.lcase) ? opts.lcase : defOpts.lcase;
+    if (!lcase && !num)
+        lcase = true;
+    const chars = [];
+    if (lcase)
+        chars.push(...list.lcase);
+    if (ucase)
+        chars.push(...list.ucase);
+    if (num)
+        chars.push(...list.num);
+    if (sym)
+        chars.push(...list.sym);
+    if (exclSimilarChars) {
+        chars.splice(chars.indexOf('l'), 1);
+        chars.splice(chars.indexOf('I'), 1);
+        chars.splice(chars.indexOf('1'), 1);
+        chars.splice(chars.indexOf('o'), 1);
+        chars.splice(chars.indexOf('O'), 1);
+        chars.splice(chars.indexOf('0'), 1);
     }
-    return shuffleArray(pwd).join('');
-  }
-  return Array(len).fill(null).map(function () {
-    return getRandChar(chars.join(''));
-  }).join('');
+    if (strict) {
+        const pwd = [];
+        if (lcase)
+            pwd.push(getRandChar(list.lcase));
+        if (ucase)
+            pwd.push(getRandChar(list.ucase));
+        if (num)
+            pwd.push(getRandChar(list.num));
+        if (sym)
+            pwd.push(getRandChar(list.sym));
+        for (let i = pwd.length; i < len; i++) {
+            pwd.push(getRandChar(chars.join('')));
+        }
+        return shuffleArray(pwd).join('');
+    }
+    return Array(len)
+        .fill(null)
+        .map(() => getRandChar(chars.join('')))
+        .join('');
 }
 function getRandChar(str) {
-  return str.charAt(Math.floor(Math.random() * str.length));
+    return str.charAt(Math.floor(Math.random() * str.length));
 }
 function shuffleArray(a) {
-  for (var i = a.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1));
-    var _ref = [a[j], a[i]];
-    a[i] = _ref[0];
-    a[j] = _ref[1];
-  }
-  return a;
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
 }
 
 export { compare, create, encrypt, getDigest, getDigests, getKeyLen, getSaltRounds, setDigest, setKeyLen, setSaltRounds };
